2621d639451e4e59ef460b39dfaeac76ce228c91
Hai <wanghai@uestc.edu.cn>
2022-10-03 00:06:23 +0800

    update license

--- benchmarks/gdp_thermal_matrices/A_mat_gen.m
+++ benchmarks/gdp_thermal_matrices/A_mat_gen.m
@@ -1,3 +1,22 @@
+%
+% This file is part of GDP.
+% Copyright (C) 2022 Hai Wang and Qinhui Yang.
+%
+% GDP is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% GDP is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+% or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+% License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with GDP. If not, see <https://www.gnu.org/licenses/>.
+%
+
+
 % This script generates the A and A_bar matrices used in GDP. A is used for
 % the steady GDP and A_bar is used for transient GDP. 
 
@@ -5,7 +24,8 @@
 % be the same as the dvfs_epoch in base.cfg: for example, t_budget = 0.001
 % and dvfs_epoch = 1000000 means power budget is updated every 1ms.
 
-% G, C, B matrices need to extracted from HotSpot with a floorplan first.
+% G, C, B matrices need to extracted from HotSpot or some thermal
+% modeling tool (not shiped with GDP) with a floorplan first.
 
 name_of_chip = '8x8_manycore';
 % set the power budgeting time step, works for transient GDP (A_bar)
